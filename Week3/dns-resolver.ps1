eaf0b0f11bd342251dd9a39f2e7e06afeacfd7b8
Adam-Hachem <112991444+Adam-Hachem@users.noreply.github.com>
2023-09-18 22:05:31 -0400

    Add files via upload

---
+++ Week3/dns-resolver.ps1
@@ -0,0 +1,8 @@
+﻿param($prefix, $dns)
+for ($ip = 1; $ip -le 254; $ip++) {
+    $addr = "$prefix.$ip"
+    $response = Resolve-DnsName -DnsOnly $addr -Server $dns -ErrorAction Ignore
+    if ($response) {
+        Write-Host "$addr $($response.NameHost)"
+    }
+}
